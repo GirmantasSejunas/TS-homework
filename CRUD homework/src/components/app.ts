3c6c15697ba37823f6a34b33987b5469767ba53f
Girmantas <girmantas123@gmail.com>
2022-08-29 07:11:09 +0300

    13 lesson homework

--- CRUD homework/src/components/app.ts
+++ CRUD homework/src/components/app.ts
@@ -3,41 +3,91 @@ import cars from '../data/cars';
 import brands from '../data/brands';
 import models from '../data/models';
 import CarsCollection from '../helpers/cars-colection';
-import stringifyProps from '../helpers/stingify-object';
+import stringifyProps, { StringifyObjectProps } from '../helpers/stingify-object';
+import CarJoined from '../types/car-joined';
+import SelectField from './select-field';
 
 class App {
-    private htmlElement: HTMLElement;
+  private carsCollection: CarsCollection;
 
-    private carsColections: CarsCollection;
+  private selectedBrandId: null | string;
 
-    constructor(selector: string) {
-      const foundElement = document.querySelector<HTMLElement>(selector);
-      this.carsColections = new CarsCollection({ cars, brands, models });
+  private brandSelect: SelectField;
 
-      if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
+  private carTable: Table<StringifyObjectProps<CarJoined>>;
 
-      this.htmlElement = foundElement;
-    }
+  private htmlElement: HTMLElement;
 
-    initialize = (): void => {
-      const carTable = new Table({
-        title: 'All Cars',
-        colums: {
-          id: 'Id',
-          brand: 'Brand',
-          model: 'model',
-          price: 'Price',
-          year: 'Year',
-        },
-        rowData: this.carsColections.all.map(stringifyProps),
+  public constructor(selector: string) {
+    const foundElement = document.querySelector<HTMLElement>(selector);
+    if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
-      });
-      const container = document.createElement('div');
-      container.className = 'container-5';
-      container.appendChild(carTable.htmlElement);
+    this.carsCollection = new CarsCollection({ cars, brands, models });
+    this.carTable = new Table({
+      title: 'All Cars',
+      columns: {
+        id: 'Id',
+        brand: 'Brand',
+        model: 'Model',
+        price: 'Price',
+        year: 'Year',
+      },
+      rowsData: this.carsCollection.all.map(stringifyProps),
+      onDelete: this.handleCarDelete,
+    });
+    this.brandSelect = new SelectField({
+      labelText: 'brand',
+      options: brands.map(({ id, title }) => ({ title, value: id })),
+      onChange: this.handleBrandChange,
+    });
+    this.selectedBrandId = null;
 
-      this.htmlElement.append(container);
-    };
+    this.htmlElement = foundElement;
+
+    this.initialize();
   }
 
-  export default App;
+  private handleBrandChange = (brandId: string): void => {
+    this.selectedBrandId = brandId;
+
+    this.update();
+  };
+
+  private handleCarDelete = (carId: string): void => {
+    this.carsCollection.deleteCarById(carId);
+
+    this.update();
+  };
+
+  private update = (): void => {
+    const { selectedBrandId, carsCollection } = this;
+
+    if (selectedBrandId === null) {
+      this.carTable.updateProps({
+        title: 'All Cars',
+        rowsData: carsCollection.all.map(stringifyProps),
+      });
+    } else {
+      const brand = brands.find((b) => b.id === selectedBrandId);
+      if (brand === undefined) throw new Error('there is no exists brand');
+
+      this.carTable.updateProps({
+        title: `${brand.title} cars`,
+        rowsData: carsCollection.getByBrandId(selectedBrandId).map(stringifyProps),
+      });
+    }
+  };
+
+  public initialize = (): void => {
+    const container = document.createElement('div');
+    container.className = 'container my-4 d-flex  flex-column gap-3';
+    container.append(
+      this.brandSelect.htmlElement,
+      this.carTable.htmlElement,
+    );
+
+    this.htmlElement.append(container);
+  };
+}
+
+export default App;
